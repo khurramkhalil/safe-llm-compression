5296bab047f7a3be9a035c51c76578a7bbf83039
Khurram Khalil <khurram.khalil@missouri.edu>
2025-03-11 17:06:40 -0500

    Trying to fix memory issues still......

--- scripts/run_optimization.py
+++ scripts/run_optimization.py
@@ -3,6 +3,7 @@ import os
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, project_root)
 
+import numpy as np
 import torch
 import yaml
 import csv
